c258348c0a6be3d4eb737d5c9663ba846b2c4691
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-24 21:05:18 +0530

    done

---
+++ Bit manupulation/Binary_OR.java
@@ -0,0 +1,30 @@
+/*
+Binary OR Operator (|):
+Rules:
+1|1=1
+1|0=1
+0|1=1
+0|0=0
+Example:
+5|3
+5=101
+3=011
+    -----
+    111=7
+
+
+*/
+import java.util.*;
+public class Binary_OR {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b=sc.nextInt();
+        int result=a|b;
+        System.out.println("The result of "+a+" | "+b+" is: "+result);
+        sc.close();
+    }
+    
+}
