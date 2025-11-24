c95deacce720b9d885331ee4b5466d788836be1e
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-24 20:57:54 +0530

    done

---
+++ Bit manupulation/intro_to_bit_man.java
@@ -0,0 +1,5 @@
+//How information in computer is stored, it is stored in bits(0s and 1s).
+//Bit manipulation is the process of algorithmically manipulating bits or binary digits, which are the most basic form of data in computing and digital communications.
+//Decimal number system is base 10, it uses 10 digits from 0 to 9.
+//Binary number system is base 2, it uses only 2 digits 0 and 1.
+//In decimal system 999 is written as just 999 but in binary it is written as 1111100111.
