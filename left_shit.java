f103647523e02799f733db8dc79a33e833386424
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-24 21:40:58 +0530

    done

---
+++ left_shit.java
@@ -0,0 +1,30 @@
+/*
+left shift operator (<<):
+rules:
+1)When we left shift a number by n positions, it is equivalent to multiplying the number by 2^n.
+2)Each left shift operation effectively adds a 0 to the right end of the binary representation of the number.
+Example:
+5<<1
+5=101
+    -----
+   1010=10
+5<<2
+5=101
+    -----
+  10100=20
+5<<3
+5=101
+    -----
+ 101000=40
+2<<3
+2=10
+    -----
+    10000=16
+
+*/
+
+
+
+public class left_shit {
+    
+}
