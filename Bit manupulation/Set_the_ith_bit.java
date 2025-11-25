6467f9cfb75ff19141876b14506f74fec4acba96
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-25 13:34:49 +0530

    done

--- Bit manupulation/Set_the_ith_bit.java
+++ Bit manupulation/Set_the_ith_bit.java
@@ -4,10 +4,7 @@
 public class Set_the_ith_bit {
     public static int setIthBit(int n, int i){
         int bitMask = 1 << i;
-        if((n & bitMask) == 0){
-            return 0; 
-        }
-        return 1; 
+        return n | bitMask;
     }
     public static void main(String[] args){
         System.out.println(setIthBit(5, 1)); // Example usage
